ac7a8f125eddc4aebc17dd22b42da56373c56229
khasdragon@tutanota.com <khasdragon@tutanota.com>
2024-01-18 05:02:58 +0000

    Created Globals and NewTaskModal.RowTemplate2; Edited NewTaskModal and CRUD_Functions

---
+++ client_code/Globals.py
@@ -0,0 +1,6 @@
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+kanban_types = ['backlog', 'progress', 'review', 'done']
--- server_code/CRUD_Functions.py
+++ server_code/CRUD_Functions.py
@@ -1,4 +1,5 @@
 import anvil.tables as tables
+import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
