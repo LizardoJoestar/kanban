edb3057d5e2936cc5213e06a27bd2cb830a5691e
LizardoJoestar <khasdragon@tutanota.com>
2024-02-02 15:23:00 -0800

    Added logout

--- client_code/LogoutScreen/__init__.py
+++ client_code/LogoutScreen/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import Startup
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -15,4 +16,5 @@ class Form1(Form1Template):
 
   def login_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    Startup.startup()
+    open_form("Index")
