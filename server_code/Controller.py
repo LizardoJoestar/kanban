8f52a53a29db525cb9a0dfc7d646d3c154b6a8e6
LizardoJoestar <khasdragon@tutanota.com>
2024-02-02 13:57:28 -0800

    Added controller conditionals

--- server_code/Controller.py
+++ server_code/Controller.py
@@ -7,23 +7,23 @@ import anvil.server
 
 # CRUD functions for Kanban tasks
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def addTask(name, desc, dueDate, status, project):
   app_tables.task.add_row(name=name, description=desc, dueDate=dueDate, status=status, project=project)
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def deleteTask(taskID):
   if app_tables.task.get_by_id(taskID) is not None:
     app_tables.task.get_by_id(taskID).delete()
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def updateTask(taskID, name, desc, dueDate, status):
   app_tables.task.get_by_id(taskID)['name'] = name
   app_tables.task.get_by_id(taskID)['description'] = desc
   app_tables.task.get_by_id(taskID)['dueDate'] = dueDate
   app_tables.task.get_by_id(taskID)['status'] = status
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def getTasks(project):
   # Get all tasks associated with a single project
   return app_tables.task.get(project=project)
