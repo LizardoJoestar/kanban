665d211296fc0d0a1870e29846b32d6c356226f6
LizardoJoestar <khasdragon@tutanota.com>
2024-02-02 15:19:49 -0800

    Added logout

--- client_code/Index/__init__.py
+++ client_code/Index/__init__.py
@@ -1,11 +1,11 @@
 from ._anvil_designer import IndexTemplate
-from anvil import *
+from anvil import * # this imports all forms
 import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..KanbanBoard import KanbanBoard
+from ..KanbanBoard import KanbanBoard # if you want to initialize a form as an object, call it like this (go to upper directory and import)
 
 class Index(IndexTemplate):
   def __init__(self, **properties):
