ffe55f08d86260110edef7f54f9953972cf84ebf
LizardoJoestar <khasdragon@tutanota.com>
2024-02-02 18:29:48 -0800

    Added

--- client_code/Index/__init__.py
+++ client_code/Index/__init__.py
@@ -11,9 +11,12 @@ class Index(IndexTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(KanbanBoard())
+
     # Any code you write here will run before the form opens.
-    # Minotaur hotel
+    self.content_panel.add_component(KanbanBoard())
+    
+    # Set current project in view
+
 
   def link_logout_click(self, **event_args):
     """This method is called when the link is clicked"""
