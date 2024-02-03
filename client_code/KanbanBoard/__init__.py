cb573455550ec8a409b723548e4dc5b2b04fab86
LizardoJoestar <khasdragon@tutanota.com>
2024-02-02 16:11:57 -0800

    Added taskmodal

--- client_code/KanbanBoard/__init__.py
+++ client_code/KanbanBoard/__init__.py
@@ -5,6 +5,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..NewTaskModal import NewTaskModal
 
 class KanbanBoard(KanbanBoardTemplate):
   def __init__(self, **properties):
--- client_code/LogoutScreen/__init__.py
+++ client_code/LogoutScreen/__init__.py
@@ -12,8 +12,6 @@ class LogoutScreen(LogoutScreenTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
   def login_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     Startup.startup()
